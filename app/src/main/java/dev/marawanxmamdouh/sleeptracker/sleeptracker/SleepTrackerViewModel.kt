4d754e154b39d866e2e2d4e0a9ef58d38a2813dc
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-07-25 12:55:33 +0200

    Use coroutines to implement the app's database calls and display the sleep data.

--- app/src/main/java/dev/marawanxmamdouh/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/dev/marawanxmamdouh/sleeptracker/sleeptracker/SleepTrackerViewModel.kt
@@ -15,5 +15,62 @@ class SleepTrackerViewModel(
     application: Application
 ) : AndroidViewModel(application) {
 
+    private var tonight = MutableLiveData<SleepNight?>()
+    private val nights = database.getAllNights()
+    val nightsString = Transformations.map(nights) { nights ->
+        formatNights(nights, application.resources)
+    }
+
+    init {
+        initializeTonight()
+    }
+
+    private fun initializeTonight() {
+        viewModelScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+        var night = database.getTonight()
+
+        if (night?.endTimeMilli != night?.startTimeMilli) {
+            night = null
+        }
+        return night
+    }
+
+    fun onStartTracking() {
+        viewModelScope.launch {
+            val newNight = SleepNight()
+            insert(newNight)
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun insert(night: SleepNight) = database.insert(night)
+
+    fun onStopTracking() {
+        viewModelScope.launch {
+            val oldNight = tonight.value ?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+        }
+    }
+
+    private suspend fun update(night: SleepNight) {
+        database.update(night)
+    }
+
+    fun onClear() {
+        viewModelScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    private suspend fun clear() {
+        database.clear()
+    }
 }
 
