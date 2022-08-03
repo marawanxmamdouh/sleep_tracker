9e8c62f51ea8049fb992a6a3e614b8e37dfed915
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-03 12:47:18 +0200

    Refactor onBindViewHolder

--- app/src/main/java/dev/marawanxmamdouh/sleeptracker/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/dev/marawanxmamdouh/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -24,21 +24,7 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        val res = holder.itemView.context.resources
-        holder.sleepLength.text =
-            convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-        holder.quality.text = convertNumericQualityToString(item.quality, res)
-        holder.qualityImage.setBackgroundResource(
-            when (item.quality) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.sleep_active
-            }
-        )
+        holder.bind(item)
 
     }
 
@@ -52,5 +38,23 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
         val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
         val quality: TextView = itemView.findViewById(R.id.quality_string)
         val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
+
+        fun bind(item: SleepNight) {
+            val res = itemView.context.resources
+            sleepLength.text =
+                convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
+            quality.text = convertNumericQualityToString(item.quality, res)
+            qualityImage.setBackgroundResource(
+                when (item.quality) {
+                    0 -> R.drawable.ic_sleep_0
+                    1 -> R.drawable.ic_sleep_1
+                    2 -> R.drawable.ic_sleep_2
+                    3 -> R.drawable.ic_sleep_3
+                    4 -> R.drawable.ic_sleep_4
+                    5 -> R.drawable.ic_sleep_5
+                    else -> R.drawable.sleep_active
+                }
+            )
+        }
     }
 }
