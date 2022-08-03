6ce51e390587e3c75a98b5134b1e486da3960e0f
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-03 15:35:36 +0200

    Create new class SleepNightDiffCallback

--- app/src/main/java/dev/marawanxmamdouh/sleeptracker/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/dev/marawanxmamdouh/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import dev.marawanxmamdouh.sleeptracker.R
 import dev.marawanxmamdouh.sleeptracker.convertDurationToFormatted
@@ -61,5 +62,15 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
                 return ViewHolder(view)
             }
         }
+    }
+}
+
+class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
+    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem.nightId == newItem.nightId
+    }
+
+    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem == newItem
     }
 }
