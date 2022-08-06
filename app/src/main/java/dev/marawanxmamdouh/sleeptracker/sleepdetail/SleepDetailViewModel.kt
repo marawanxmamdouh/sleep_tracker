33ec574ec008535ac007d9f2adcc395ff8888683
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2022-08-06 19:29:32 +0200

    Create sleep details fragment and view model

---
+++ app/src/main/java/dev/marawanxmamdouh/sleeptracker/sleepdetail/SleepDetailViewModel.kt
@@ -0,0 +1,37 @@
+package dev.marawanxmamdouh.sleeptracker.sleepdetail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dev.marawanxmamdouh.sleeptracker.database.SleepDatabaseDao
+import dev.marawanxmamdouh.sleeptracker.database.SleepNight
+
+class SleepDetailViewModel(
+    private val sleepNightKey: Long = 0L,
+    dataSource: SleepDatabaseDao
+) : ViewModel() {
+
+    val database = dataSource
+
+    private val night = MediatorLiveData<SleepNight>()
+
+    fun getNight() = night
+
+    init {
+        night.addSource(database.getNightWithId(sleepNightKey), night::setValue)
+    }
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+    fun doneNavigating() {
+        _navigateToSleepTracker.value = null
+    }
+
+    fun onClose() {
+        _navigateToSleepTracker.value = true
+    }
+}
